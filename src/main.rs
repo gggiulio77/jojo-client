5a19a2118ed0d74ec451dee41b0189291c159881
ggPersonal <gggiulio77@gmail.com>
2023-07-01 03:28:45 -0300

    feat: add Stick mod and refactor task instances

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 pub mod button;
 pub mod led;
+pub mod stick;
 pub mod websocket;
 pub mod wifi;
 
 use crossbeam_channel::bounded;
-use esp_idf_hal::{
-    gpio::{IOPin, PinDriver, Pull},
-    prelude::Peripherals,
-};
+use esp_idf_hal::prelude::Peripherals;
 use esp_idf_svc::{
     eventloop::EspSystemEventLoop, log::EspLogger, nvs::EspDefaultNvsPartition,
     timer::EspTaskTimerService,
@@ -17,6 +15,11 @@ use log::*;
 use parking_lot::{Condvar, Mutex};
 use rgb::RGB8;
 use std::sync::Arc;
+
+use crate::{
+    button::ButtonTask,
+    stick::{StickRead, StickTask},
+};
 
 const SSID: &'static str = env!("SSID");
 const PASSWORD: &'static str = env!("PASSWORD");
@@ -34,18 +37,14 @@ fn main() -> anyhow::Result<()> {
 
     info!("Starting application");
 
-    // Config pin
-    let mut btn_pin = PinDriver::input(peripherals.pins.gpio0.downgrade()).unwrap();
-    btn_pin.set_pull(Pull::Down).unwrap();
-
     // TODO: find a way (if possible) to copy/clone the TxRmtDriver instance, create a task with a channel or use this as a singleton
     // another way is to use a mutex with the neopixel instance and clone it to each task
-    let mut neopixel =
-        led::Neopixel::new(peripherals.pins.gpio48, peripherals.rmt.channel0).unwrap();
+    let mut neopixel = led::Neopixel::new(peripherals.pins.gpio48, peripherals.rmt.channel0)?;
 
-    neopixel.set(RGB8 { r: 0, g: 0, b: 0 }).unwrap();
+    neopixel.set(RGB8 { r: 0, g: 0, b: 0 })?;
     // Create channel
-    let (bt_tx, bt_rx) = bounded::<bool>(10);
+    let (bt_tx, _bt_rx) = bounded::<bool>(10);
+    let (stick_tx, stick_rx) = bounded::<StickRead>(10);
 
     let wifi_status = Arc::new((Mutex::new(false), Condvar::new()));
     let wifi_status_wb = Arc::clone(&wifi_status);
@@ -53,33 +52,37 @@ fn main() -> anyhow::Result<()> {
     let _wifi_thread = std::thread::Builder::new()
         .stack_size(6 * 1024)
         .spawn(move || {
-            wifi::connect_task(wifi::ConnectTask {
-                modem: peripherals.modem,
+            wifi::connect_task(wifi::ConnectTask::new(
+                peripherals.modem,
                 sys_loop,
-                nvs: Some(nvs),
-                timer: timer_service,
-                status: wifi_status,
-                ssid: SSID,
-                password: PASSWORD,
-            })
-        })
-        .unwrap();
+                Some(nvs),
+                timer_service,
+                wifi_status,
+                SSID,
+                PASSWORD,
+            ))
+        })?;
 
     let _button_thread = std::thread::Builder::new()
         .stack_size(4 * 1024)
-        .spawn(|| button::init_task(btn_pin, bt_tx))
-        .unwrap();
+        .spawn(|| button::init_task(ButtonTask::new(peripherals.pins.gpio0, bt_tx)))?;
 
-    let _websocket_thread = std::thread::Builder::new()
-        .stack_size(8 * 1024)
-        .spawn(|| {
-            websocket::init_task(websocket::WebsocketTask {
-                address: WEBSOCKET_ADDRESS,
-                wifi_status: wifi_status_wb,
-                bt_rx,
-            })
-        })
-        .unwrap();
+    let _stick_thread = std::thread::Builder::new().stack_size(4 * 1024).spawn(|| {
+        stick::init_task(StickTask::new(
+            peripherals.adc1,
+            peripherals.pins.gpio5,
+            peripherals.pins.gpio6,
+            stick_tx,
+        ))
+    })?;
+
+    let _websocket_thread = std::thread::Builder::new().stack_size(8 * 1024).spawn(|| {
+        websocket::init_task(websocket::WebsocketTask::new(
+            WEBSOCKET_ADDRESS,
+            wifi_status_wb,
+            stick_rx,
+        ))
+    })?;
 
     Ok(())
 }
