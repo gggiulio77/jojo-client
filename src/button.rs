3d28b6822f7ed3f4297ad9909f3c420a9cbd885a
ggPersonal <gggiulio77@gmail.com>
2023-07-01 03:27:44 -0300

    feat: add ButtonTask

--- src/button.rs
+++ src/button.rs
@@ -1,14 +1,34 @@
 use std::time::Duration;
 
-use esp_idf_hal::gpio::{AnyIOPin, Input, PinDriver};
+use esp_idf_hal::gpio::{AnyIOPin, Gpio0, IOPin, Input, PinDriver, Pull};
 use log::*;
 
-pub fn init_task(btn_pin: PinDriver<AnyIOPin, Input>, tx: crossbeam_channel::Sender<bool>) {
+pub struct ButtonTask {
+    gpio_0: Gpio0,
+    bt_tx: crossbeam_channel::Sender<bool>,
+}
+
+impl ButtonTask {
+    pub fn new(gpio_0: Gpio0, bt_tx: crossbeam_channel::Sender<bool>) -> Self {
+        ButtonTask { gpio_0, bt_tx }
+    }
+}
+
+fn init_button(btn_pin: Gpio0) -> anyhow::Result<PinDriver<'static, AnyIOPin, Input>> {
+    // Config pin
+    let mut btn = PinDriver::input(btn_pin.downgrade())?;
+    btn.set_pull(Pull::Down)?;
+
+    return Ok(btn);
+}
+
+pub fn init_task(task: ButtonTask) {
     info!("[button_task]:creating");
+    let btn = init_button(task.gpio_0).unwrap();
     let mut button_switch = false;
 
     loop {
-        match btn_pin.get_level() {
+        match btn.get_level() {
             esp_idf_hal::gpio::Level::High => {
                 if button_switch == true {
                     button_switch = false;
@@ -19,7 +39,7 @@ pub fn init_task(btn_pin: PinDriver<AnyIOPin, Input>, tx: crossbeam_channel::Sen
                 if button_switch == false {
                     button_switch = true;
                     info!("[button_task]:BUTTON PRESS");
-                    tx.send(button_switch).unwrap();
+                    task.bt_tx.send(button_switch).unwrap();
                 }
             }
         }
