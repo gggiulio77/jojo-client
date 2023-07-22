67ff875dc3185c1c7761965cda6caf9f23975cd9
ggPersonal <gggiulio77@gmail.com>
2023-07-22 19:38:40 -0300

    feat: replace wifi_status with a discovery channel, add discovery state

--- src/client/websocket.rs
+++ src/client/websocket.rs
@@ -1,16 +1,16 @@
-use std::{net::TcpStream, sync::Arc};
+use std::net::{SocketAddr, TcpStream};
 
 use bus::BusReader;
 use esp_idf_hal::delay::FreeRtos;
 use log::*;
-use parking_lot::{Condvar, Mutex};
+
 use serde::{Deserialize, Serialize};
 use tungstenite::{client, Message, WebSocket};
 use url::Url;
 
 pub struct WebsocketTask<'a> {
     address: &'a str,
-    wifi_status: Arc<(Mutex<bool>, Condvar)>,
+    discovery_rx: crossbeam_channel::Receiver<SocketAddr>,
     stick_rx: crossbeam_channel::Receiver<MouseRead>,
     bt_rx: BusReader<bool>,
 }
@@ -18,13 +18,13 @@ pub struct WebsocketTask<'a> {
 impl<'a> WebsocketTask<'a> {
     pub fn new(
         address: &'a str,
-        wifi_status: Arc<(Mutex<bool>, Condvar)>,
+        discovery_rx: crossbeam_channel::Receiver<SocketAddr>,
         stick_rx: crossbeam_channel::Receiver<MouseRead>,
         bt_rx: BusReader<bool>,
     ) -> Self {
         WebsocketTask {
             address,
-            wifi_status,
+            discovery_rx,
             stick_rx,
             bt_rx,
         }
@@ -60,7 +60,7 @@ struct WebsocketState<T> {
 
 impl<T> Default for WebsocketState<T> {
     fn default() -> Self {
-        WebsocketState::new(WebsocketStates::Init)
+        WebsocketState::new(WebsocketStates::Discovery)
     }
 }
 
@@ -75,48 +75,47 @@ impl<'a, T> WebsocketState<T> {
         self.current_state = state;
     }
 
-    pub fn to_init(&mut self) {
+    pub fn to_discovery(&mut self) {
+        info!("[websocket_task]:state_discovery");
+        self.to_state(WebsocketStates::Discovery);
+    }
+
+    pub fn to_init(&mut self, ip_address: SocketAddr) {
         info!("[websocket_task]:state_init");
-        self.to_state(WebsocketStates::Init);
+        self.to_state(WebsocketStates::Init(ip_address));
     }
 
-    pub fn to_connected(&mut self, socket: WebSocket<T>) {
+    pub fn to_connected(&mut self, socket: WebSocket<T>, ip_address: SocketAddr) {
         info!("[websocket_task]:state_connecting");
-        self.to_state(WebsocketStates::Connected(socket));
+        self.to_state(WebsocketStates::Connected(socket, ip_address));
     }
 
-    pub fn to_paused(&mut self) {
+    pub fn to_paused(&mut self, ip_address: SocketAddr) {
         info!("[websocket_task]:state_paused");
-        self.to_state(WebsocketStates::Paused);
+        self.to_state(WebsocketStates::Paused(ip_address));
     }
 }
 
 enum WebsocketStates<T> {
-    Init,
-    Connected(WebSocket<T>),
-    Paused,
+    Discovery,
+    Init(SocketAddr),
+    // TODO: remove SocketAddr
+    Connected(WebSocket<T>, SocketAddr),
+    // TODO: remove SocketAddr
+    Paused(SocketAddr),
 }
 
 pub fn init_task(task: WebsocketTask) {
     let WebsocketTask {
         address,
-        wifi_status,
+        discovery_rx,
         stick_rx,
         mut bt_rx,
     } = task;
 
-    let (lock, cvar) = &*wifi_status;
-
-    let mut started = lock.lock();
-
-    if !*started {
-        cvar.wait(&mut started);
-    }
-    drop(started);
-
     // Parsing url
     let parsed_url = Url::parse(address).unwrap();
-    let host = parsed_url.host_str().unwrap();
+    let _host = parsed_url.host_str().unwrap();
     let port = parsed_url.port().unwrap();
 
     let mut main_state: WebsocketState<TcpStream> = WebsocketState::default();
@@ -124,21 +123,32 @@ pub fn init_task(task: WebsocketTask) {
     loop {
         // TODO: remove current_state access, replace with something else
         match main_state.current_state {
-            WebsocketStates::Init => {
-                info!("[websocket_task]:Connecting to: {:?}", address);
-                let stream = TcpStream::connect(format!("{}:{}", host, port)).unwrap();
+            WebsocketStates::Discovery => 'label: {
+                if let Ok(ip_address) = discovery_rx.try_recv() {
+                    main_state.to_init(ip_address);
+                    break 'label;
+                }
+                FreeRtos::delay_ms(100);
+            }
+            WebsocketStates::Init(ip_address) => {
+                info!(
+                    "[websocket_task]:Connecting to: {:?}:{:?}",
+                    ip_address.ip(),
+                    port
+                );
+                let stream = TcpStream::connect(format!("{}:{}", ip_address.ip(), port)).unwrap();
 
                 let (socket, _response) = client(&parsed_url, stream).unwrap();
 
                 info!("[websocket_task]:Connected.");
 
-                main_state.to_connected(socket);
+                main_state.to_connected(socket, ip_address);
             }
-            WebsocketStates::Connected(ref mut socket) => 'label: {
+            WebsocketStates::Connected(ref mut socket, ip_address) => 'label: {
                 if let Ok(_) = bt_rx.try_recv() {
                     socket.write_message(Message::Close(None)).unwrap();
                     drop(socket);
-                    main_state.to_paused();
+                    main_state.to_paused(ip_address);
                     break 'label;
                 }
 
@@ -149,10 +159,11 @@ pub fn init_task(task: WebsocketTask) {
                     .write_message(Message::Binary(bincode::serialize(&mouse_reads).unwrap()))
                     .unwrap();
             }
-            WebsocketStates::Paused => 'label: {
+            WebsocketStates::Paused(ip_address) => 'label: {
                 // TODO: to this to work we need to pause the ADC, for this we need to signal with stick_tx the STOP and also RESUME
                 if let Ok(_) = bt_rx.try_recv() {
-                    main_state.to_init();
+                    // TODO: when we have a method to restart the discovery task replace this with to_discovery
+                    main_state.to_init(ip_address);
                     break 'label;
                 }
                 FreeRtos::delay_ms(500);
