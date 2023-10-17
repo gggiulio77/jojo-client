667899cdfb8784aeaa6816b772354b376cadd706
Gonzalo Giulio <gggiulio77@gmail.com>
2023-10-17 02:27:18 -0300

    feature: add new channels and refactor websocket task

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -13,13 +13,13 @@ use parking_lot::{Condvar, Mutex};
 use rgb::RGB8;
 
 pub mod broadcast;
-pub mod stick;
+pub mod mouse;
 pub mod websocket;
 pub mod wifi_client;
 
 use crate::{button, client::broadcast::BroadcastTask, led, NetworkCredentials};
 
-const WEBSOCKET_ADDRESS: &'static str = env!("WEBSOCKET_ADDRESS");
+const WEBSOCKET_PATH: &'static str = env!("WEBSOCKET_PATH");
 
 pub fn main(
     nvs_default: EspNvsPartition<NvsDefault>,
@@ -41,15 +41,19 @@ pub fn main(
     let bt_wb_rx = bt_bus.add_rx();
     let bt_stick_rx = bt_bus.add_rx();
 
-    let (stick_tx, stick_rx) = unbounded::<websocket::MouseRead>();
-
     let wifi_status = Arc::new((Mutex::new(false), Condvar::new()));
     let wifi_status_bd = Arc::clone(&wifi_status);
     // TODO: review this condvar, maybe replace it with a websocket broadcast channel
     let wifi_status_stick = Arc::clone(&wifi_status);
+    let wifi_status_left_click = Arc::clone(&wifi_status);
 
     // TODO: review this channel, maybe replace it with two broadcast channels, for websocket and discovery
     let (discovery_tx, discovery_rx) = unbounded::<SocketAddr>();
+
+    // Channels to send websocket messages
+    let (wb_sender_tx, wb_sender_rx) = unbounded::<jojo_common::message::ClientMessage>();
+    let stick_wb_sender_tx = wb_sender_tx.clone();
+    let left_click_wb_sender_tx = wb_sender_tx.clone();
 
     info!("[client_task]: creating tasks");
 
@@ -76,25 +80,32 @@ pub fn main(
         .spawn(|| button::init_task(button::ButtonTask::new(peripherals.pins.gpio0, bt_bus)))?;
 
     let _stick_thread = std::thread::Builder::new().stack_size(6 * 1024).spawn(|| {
-        stick::init_task(stick::StickTask::new(
+        mouse::stick::init_task(mouse::stick::StickTask::new(
             peripherals.adc1,
             peripherals.pins.gpio5,
             peripherals.pins.gpio6,
-            peripherals.pins.gpio7,
-            stick_tx,
-            bt_wb_rx,
+            // TODO: replace with stick_websocket_sender_tx
+            stick_wb_sender_tx,
             wifi_status_stick,
         ))
     })?;
 
+    let _left_click_thread = std::thread::Builder::new().stack_size(6 * 1024).spawn(|| {
+        mouse::left_click::init_task(mouse::left_click::LeftClickTask::new(
+            peripherals.pins.gpio7,
+            // TODO: replace with stick_websocket_sender_tx
+            left_click_wb_sender_tx,
+            wifi_status_left_click,
+        ))
+    })?;
+
     let _websocket_thread = std::thread::Builder::new()
-        .stack_size(12 * 1024)
+        .stack_size(20 * 1024)
         .spawn(|| {
             websocket::init_task(websocket::WebsocketTask::new(
-                WEBSOCKET_ADDRESS,
+                WEBSOCKET_PATH,
                 discovery_rx,
-                stick_rx,
-                bt_stick_rx,
+                wb_sender_rx,
             ))
         })?;
 
