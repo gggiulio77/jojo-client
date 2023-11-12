9f2722322d12f38c6fc16269bf9615bf351e94d6
Gonzalo Giulio <gggiulio77@gmail.com>
2023-11-11 21:10:43 -0300

    feat: extract actions from device and create tasks for buttons

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -5,9 +5,10 @@ use crossbeam_channel::unbounded;
 use esp_idf_hal::prelude::Peripherals;
 use esp_idf_svc::{
     eventloop::EspSystemEventLoop,
-    nvs::{EspNvsPartition, NvsDefault},
+    nvs::{EspNvs, EspNvsPartition, NvsDefault},
     timer::EspTaskTimerService,
 };
+use jojo_common::{button::ButtonAction, device::Device};
 use log::*;
 use parking_lot::{Condvar, Mutex};
 use rgb::RGB8;
@@ -17,13 +18,15 @@ pub mod mouse;
 pub mod websocket;
 pub mod wifi_client;
 
-use crate::{button, client::broadcast::BroadcastTask, led};
+use crate::{client::broadcast::BroadcastTask, led};
 
 const WEBSOCKET_PATH: &'static str = env!("WEBSOCKET_PATH");
 
 pub fn main(
     nvs_default: EspNvsPartition<NvsDefault>,
     network_credentials: jojo_common::network::NetworkCredentials,
+    nvs_namespace: EspNvs<NvsDefault>,
+    device: Device,
 ) -> anyhow::Result<()> {
     info!("[client_task]: init");
 
@@ -43,9 +46,11 @@ pub fn main(
 
     let wifi_status = Arc::new((Mutex::new(false), Condvar::new()));
     let wifi_status_bd = Arc::clone(&wifi_status);
+
     // TODO: review this condvar, maybe replace it with a websocket broadcast channel
-    let wifi_status_stick = Arc::clone(&wifi_status);
-    let wifi_status_left_click = Arc::clone(&wifi_status);
+    let wb_status = Arc::new((Mutex::new(false), Condvar::new()));
+    let wb_status_stick = Arc::clone(&wb_status);
+    let wb_status_left_click = Arc::clone(&wb_status);
 
     // TODO: review this channel, maybe replace it with two broadcast channels, for websocket and discovery
     let (discovery_tx, discovery_rx) = unbounded::<SocketAddr>();
@@ -72,42 +77,63 @@ pub fn main(
         })?;
 
     let _broadcast_discovery = std::thread::Builder::new()
-        .stack_size(4 * 1024)
+        .stack_size(6 * 1024)
         .spawn(|| broadcast::init_task(BroadcastTask::new(wifi_status_bd, discovery_tx)))?;
 
-    let _button_thread = std::thread::Builder::new()
-        .stack_size(4 * 1024)
-        .spawn(|| button::init_task(button::ButtonTask::new(peripherals.pins.gpio0, bt_bus)))?;
+    // let _button_thread = std::thread::Builder::new()
+    //     .stack_size(4 * 1024)
+    //     .spawn(|| button::init_task(button::ButtonTask::new(peripherals.pins.gpio0, bt_bus)))?;
 
-    let _stick_thread = std::thread::Builder::new().stack_size(6 * 1024).spawn(|| {
+    let actions_map = device.actions_map().clone();
+    let button_action: ButtonAction = actions_map
+        .into_values()
+        .collect::<Vec<ButtonAction>>()
+        .first()
+        .unwrap()
+        .to_owned();
+
+    info!("[client_task]: got action {:?}", button_action);
+
+    let _left_click = std::thread::Builder::new().stack_size(6 * 1024).spawn(|| {
+        mouse::left_click::init_task(mouse::left_click::LeftClickTask::new(
+            peripherals.pins.gpio7,
+            // TODO: replace with stick_websocket_sender_tx
+            left_click_wb_sender_tx,
+            wb_status_left_click,
+            button_action,
+        ))
+    })?;
+
+    let _stick_thread = std::thread::Builder::new().stack_size(8 * 1024).spawn(|| {
         mouse::stick::init_task(mouse::stick::StickTask::new(
             peripherals.adc1,
             peripherals.pins.gpio5,
             peripherals.pins.gpio6,
             // TODO: replace with stick_websocket_sender_tx
             stick_wb_sender_tx,
-            wifi_status_stick,
-        ))
-    })?;
-
-    let _left_click_thread = std::thread::Builder::new().stack_size(6 * 1024).spawn(|| {
-        mouse::left_click::init_task(mouse::left_click::LeftClickTask::new(
-            peripherals.pins.gpio7,
-            // TODO: replace with stick_websocket_sender_tx
-            left_click_wb_sender_tx,
-            wifi_status_left_click,
+            wb_status_stick,
         ))
     })?;
 
     let _websocket_thread = std::thread::Builder::new()
-        .stack_size(20 * 1024)
+        .stack_size(30 * 1024)
         .spawn(|| {
             websocket::init_task(websocket::WebsocketTask::new(
                 WEBSOCKET_PATH,
                 discovery_rx,
                 wb_sender_rx,
+                wb_status,
+                device,
             ))
         })?;
+
+    // let _ = std::thread::Builder::new().stack_size(4 * 1024).spawn(|| {
+    //     let time = Instant::now();
+    //     loop {
+    //         info!("[client_task]: i'am alive bitch {:?}", time.elapsed());
+    //         Ets::delay_ms(60000);
+    //     }
+    // })?;
 
     Ok(())
 }
