a3fba385223f3488aacad356c9b93d52cd0ccd7d
Gonzalo Giulio <gggiulio77@gmail.com>
2023-11-11 21:09:14 -0300

    feat: add condvar to propagate status and optimize read operation on stream

--- src/client/websocket.rs
+++ src/client/websocket.rs
@@ -1,10 +1,12 @@
-use esp_idf_hal::delay::FreeRtos;
+use esp_idf_hal::delay::Ets;
+use jojo_common::device::Device;
 use log::*;
+use parking_lot::{Condvar, Mutex};
 use std::{
     net::{SocketAddr, TcpStream},
     sync::Arc,
+    time::Duration,
 };
-use uuid::uuid;
 
 use tungstenite::{client::client_with_config, protocol::WebSocketConfig, Message, WebSocket};
 
@@ -12,6 +14,8 @@ pub struct WebsocketTask<'a> {
     path: &'a str,
     discovery_rx: crossbeam_channel::Receiver<SocketAddr>,
     websocket_sender_rx: crossbeam_channel::Receiver<jojo_common::message::ClientMessage>,
+    status: Arc<(Mutex<bool>, Condvar)>,
+    device: Device,
 }
 
 impl<'a> WebsocketTask<'a> {
@@ -19,11 +23,15 @@ impl<'a> WebsocketTask<'a> {
         path: &'a str,
         discovery_rx: crossbeam_channel::Receiver<SocketAddr>,
         websocket_sender_rx: crossbeam_channel::Receiver<jojo_common::message::ClientMessage>,
+        status: Arc<(Mutex<bool>, Condvar)>,
+        device: Device,
     ) -> Self {
         WebsocketTask {
             path,
             discovery_rx,
             websocket_sender_rx,
+            status,
+            device,
         }
     }
 }
@@ -79,6 +87,8 @@ pub fn init_task(task: WebsocketTask) {
         path,
         discovery_rx,
         websocket_sender_rx,
+        status,
+        device,
     } = task;
 
     let mut main_state = WebsocketState::default();
@@ -90,30 +100,31 @@ pub fn init_task(task: WebsocketTask) {
                     main_state.to_init(ip_address);
                     break 'label;
                 }
-                FreeRtos::delay_ms(100);
+                std::thread::sleep(Duration::from_millis(100));
             }
             WebsocketStates::Init(server_address) => {
                 info!(
-                    "[websocket_task]:Connecting to: {}/{}",
+                    "[websocket_task]:Connecting to: {}/{}/340917e8-87a9-455c-9645-d08eb99162f9",
                     server_address, path
                 );
 
                 let stream = TcpStream::connect(server_address).unwrap();
+                // TODO: review this approach for non blocking reads and write to the stream. If we use it, an HandshakeError::Interrupted() is thrown
+                // stream.set_nonblocking(true).unwrap();
 
-                // let (socket, _response) =
-                //     client(&format!("ws://{}/{}", server_address, path), stream).unwrap();
-                // TODO: replace uuid with device
+                // This let us reduce the "blocking" while reading from websocket
+                stream
+                    .set_read_timeout(Some(Duration::from_millis(10)))
+                    .unwrap();
+
                 let (socket, _broadcast_discovery) = client_with_config(
-                    &format!(
-                        "ws://{}/{}/340917e8-87a9-455c-9645-d08eb99162f9",
-                        server_address, path
-                    ),
+                    &format!("ws://{}/{}/{}", server_address, path, device.id()),
                     stream,
                     Some(WebSocketConfig {
-                        write_buffer_size: 64,
-                        max_message_size: Some(256),
-                        max_write_buffer_size: 256,
-                        max_frame_size: Some(256),
+                        write_buffer_size: 1024,
+                        max_message_size: Some(2048),
+                        max_write_buffer_size: 2048,
+                        max_frame_size: Some(2048),
                         accept_unmasked_frames: true,
                         ..WebSocketConfig::default()
                     }),
@@ -123,22 +134,22 @@ pub fn init_task(task: WebsocketTask) {
                 main_state.to_connected(socket);
             }
             WebsocketStates::Connected(socket) => {
+                let (lock, cvar) = &*status;
+                // Write value to mutex
+                *lock.lock() = true;
+                cvar.notify_all();
+                drop(status);
+
                 let socket_tx = Arc::new(parking_lot::Mutex::new(socket));
                 let socket_rx = socket_tx.clone();
 
                 info!("[websocket_task]:Sending Device");
-                // TODO: replace hardcoded device with value living in flash
-                let device = jojo_common::device::Device::new(
-                    uuid!("340917e8-87a9-455c-9645-d08eb99162f9"),
-                    "test".to_string(),
-                    None,
-                    vec![],
-                );
+
                 let message = jojo_common::message::ClientMessage::Device(device);
 
                 socket_tx
                     .lock()
-                    .write(Message::Binary(bincode::serialize(&message).unwrap()))
+                    .send(Message::Binary(bincode::serialize(&message).unwrap()))
                     .unwrap();
 
                 drop(message);
@@ -149,27 +160,25 @@ pub fn init_task(task: WebsocketTask) {
                 let _ = std::thread::Builder::new()
                     .stack_size(6 * 1024)
                     .spawn(move || loop {
-                        // info!("[websocket_task]: reading from websocket");
                         if let Ok(message) = socket_rx.lock().read() {
                             // info!("[websocket_task]:Rx: {:?}", message);
                             message_handler(message)
                         }
-                        FreeRtos::delay_ms(750);
+                        std::thread::sleep(Duration::from_millis(750));
                     })
                     .unwrap();
 
                 info!("[websocket_task]: init write task");
 
-                // Task to write to websocket
                 let _ = std::thread::Builder::new()
-                    .stack_size(6 * 1024)
+                    .stack_size(22 * 1024)
                     .spawn(move || loop {
                         // info!("[websocket_task]: writing to websocket");
                         if let Ok(reads) = websocket_sender_rx.try_recv() {
                             // info!("[websocket_task]:Tx : {:?}", reads);
                             socket_tx
                                 .lock()
-                                .write(Message::Binary(bincode::serialize(&reads).unwrap()))
+                                .send(Message::Binary(bincode::serialize(&reads).unwrap()))
                                 .unwrap();
                         } else {
                             // TODO: this is to maintain a "flow" feeling, review why this happen
@@ -178,20 +187,23 @@ pub fn init_task(task: WebsocketTask) {
                             ]);
                             socket_tx
                                 .lock()
-                                .write(Message::Binary(bincode::serialize(&empty_message).unwrap()))
+                                .send(Message::Binary(bincode::serialize(&empty_message).unwrap()))
                                 .unwrap();
                         }
+                        // std::thread::sleep(Duration::from_millis(1));
+                        Ets::delay_us(250);
                     })
                     .unwrap();
 
                 loop {
-                    FreeRtos::delay_ms(1000);
+                    std::thread::sleep(Duration::from_millis(100));
                 }
             }
         }
     }
 }
 
-pub fn message_handler(_message: Message) {
-    ()
+// TODO: write this handler, we must manage updating the actions in the device and save it to flash
+pub fn message_handler(message: Message) {
+    info!("[message_handler]: {:?}", message);
 }
